bbffe40cbf1a37efca67b6a73841ad5ab7739621
Sasha Naykus <sasha.Naykus@gmail.com>
2024-07-09 18:45:59 +0300

    Add project files.

---
+++ Rogue-C/Rogue-C.cpp
@@ -0,0 +1,23 @@
+﻿// Rogue-C.cpp : Defines the entry point for the application.
+//
+
+#include "Rogue-C.h"
+#include "./include/raylib.h"
+
+using namespace std;
+
+int main()
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow(); 
+    return 0;
+}
