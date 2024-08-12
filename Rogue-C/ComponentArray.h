f634b0dcb6830e6381073d0586c5a72fe5f8b56c
TheFikus <sasha.naykus@gmail.com>
2024-08-12 16:09:44 +0300

    added profiling tooling

--- Rogue-C/ComponentArray.h
+++ Rogue-C/ComponentArray.h
@@ -13,16 +13,19 @@ template<typename T>
 class ComponentArray : public IComponentArray {
 public:
 	ComponentArray() {
-		// for(Entity i = 0; i < MAX_ENTITIES; i++) {
-		// 	_sparseToDense[i] = MAX_ENTITIES;
-		// 	_denseToSparse[i] = MAX_ENTITIES;
-		// }
+#if ARRAY_COMPONENT_ARRAYS
+		for(Entity i = 0; i < MAX_ENTITIES; i++) {
+			_sparseToDense[i] = MAX_ENTITIES;
+			_denseToSparse[i] = MAX_ENTITIES;
+		}
+#endif
 		_components.reserve(MAX_ENTITIES);
 	}
 
 	void AddComponent(Entity entity, T component) {
 		_sparseToDense[entity] = _size;
 		_denseToSparse[_size] = entity;
+#ifdef MAP_COMPONENT_ARRAYS
 		if(_size >= _components.size())
 		{
 			_components.push_back(component);
@@ -31,11 +34,18 @@ public:
 		{
 			_components[_size] = component;
 		}
+#elif ARRAY_COMPONENT_ARRAYS
+		_components[_size] = component;
+#endif
 		_size++;
 	}
 	
 	void RemoveComponent(Entity entity) {
+#ifdef MAP_COMPONENT_ARRAYS
 		ASSERT(_sparseToDense.contains(entity), "There is no such component on this entity");
+#elif ARRAY_COMPONENT_ARRAYS
+		ASSERT(_sparseToDense[entity] < MAX_ENTITIES, "There is no such component on this entity");
+#endif
 		Entity indexOfRemoved = _sparseToDense[entity];
 		Entity indexOfLast = _size - 1;
 		Entity entityOfLast = _denseToSparse[indexOfLast];
@@ -45,11 +55,14 @@ public:
 		_sparseToDense[entityOfLast] = indexOfRemoved;
 		_denseToSparse[indexOfRemoved] = entityOfLast;
 		
-		// _sparseToDense[entity] = MAX_ENTITIES;
-		// _denseToSparse[entityOfLast] = MAX_ENTITIES;
-		
+
+#ifdef MAP_COMPONENT_ARRAYS
 		_sparseToDense.erase(entity);
 		_denseToSparse.erase(entityOfLast);
+#elif ARRAY_COMPONENT_ARRAYS
+		_sparseToDense[entity] = MAX_ENTITIES;
+		_denseToSparse[entityOfLast] = MAX_ENTITIES;
+#endif
 
 		_size--;
 	}
@@ -60,15 +73,27 @@ public:
 	}
 	
 	void EntityDestroyed(Entity entity) override {
+#ifdef MAP_COMPONENT_ARRAYS
 		if(_sparseToDense.contains(entity) == false) {
 			return;
 		}
+#elif ARRAY_COMPONENT_ARRAYS
+		if(_sparseToDense[entity] >= MAX_ENTITIES) {
+			return;
+		}
+#endif
 		RemoveComponent(entity);
 	}
 
 private:
+#ifdef MAP_COMPONENT_ARRAYS
 	std::unordered_map<Entity, Entity> _sparseToDense{ MAX_ENTITIES };
 	std::unordered_map<Entity, Entity> _denseToSparse{ MAX_ENTITIES };
+#elif ARRAY_COMPONENT_ARRAYS
+	std::array<Entity, MAX_ENTITIES> _sparseToDense{ MAX_ENTITIES };
+	std::array<Entity, MAX_ENTITIES> _denseToSparse{ MAX_ENTITIES };
+#endif
+
 	std::vector<T> _components;
 	Entity _size = 0;
 };
--- Rogue-C/src/System.cpp
+++ Rogue-C/src/System.cpp
@@ -53,18 +53,24 @@ void SystemManager::HandleCollision(const Collision2D& collision, const Signatur
 
 void SystemManager::Update(float dt) {
     for (auto const& pair : _systems) {
+        auto updateClock = std::chrono::high_resolution_clock::now();
         pair.second->Update(dt);
+        _updateTimings[pair.first] = (std::chrono::high_resolution_clock::now() - updateClock).count();
     }
 }
 
 void SystemManager::PhysicsUpdate(float dt) {
     for (auto const& pair : _systems) {
+        auto updateClock = std::chrono::high_resolution_clock::now();
         pair.second->PhysicsUpdate(dt);
+        _physUpdateTimings[pair.first] = (std::chrono::high_resolution_clock::now() - updateClock).count();
     }
 }
 
 void SystemManager::Draw() {
     for (auto const& pair : _systems) {
+        auto updateClock = std::chrono::high_resolution_clock::now();
         pair.second->Draw();
+        _drawTimings[pair.first] = (std::chrono::high_resolution_clock::now() - updateClock).count();
     }
 }
