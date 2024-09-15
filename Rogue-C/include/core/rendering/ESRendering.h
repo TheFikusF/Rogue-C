7d04fc7657e98bd08846864f2d8efae5b4e223b9
TheFikus <sasha.naykus@gmail.com>
2024-09-15 12:48:57 +0300

    rendering is going hard

--- Rogue-C/include/core/rendering/ESRendering.h
+++ Rogue-C/include/core/rendering/ESRendering.h
@@ -1,19 +1,29 @@
 #pragma once
+#include "./include/core/rendering/SpriteManager.h"
+#include "./include/core/Transform.h"
 #include <cstdint>
 #include <vector>
 #include <array>
 #include <map>
 
 namespace Rendering {
+    struct ThingToRender {
+        SpriteID what;
+        MTransform where;
+        Color tint;
+
+        ThingToRender(SpriteID id, const MTransform& tr, Color tint) : what(id), where(tr), tint(tint) { }
+    };
+
     class ESRenderer {
     private:
         std::uint8_t currentQueue = 0;
-        std::array<std::map<float, std::vector<void (*) (void)>>, 2> renderQueues;
+        std::array<std::map<float, std::vector<ThingToRender>>, 2> renderQueues;
 
-        static std::uint8_t GetNextQueue();
+        std::uint8_t GetNextQueue();
 
     public:
-        static void PushRenderFunc(float order, void (*func) (void));
+        static void PushRenderFunc(float order, SpriteID sprite, const MTransform& transform, Color tint);
         static void Draw();
         static void Sync();
     };
--- Rogue-C/src/ESRendering.cpp
+++ Rogue-C/src/ESRendering.cpp
@@ -1,3 +1,37 @@
 #include "./include/core/rendering/ESRendering.h"
 
 // TODO: Change callback towards storage of SpriteIDs and MTransforms and call it something like a "draw call"
+using namespace Rendering;
+
+static ESRenderer renderer;
+
+std::uint8_t ESRenderer::GetNextQueue() {
+    return ((renderer.currentQueue + 1) >= 2) ? 0 : renderer.currentQueue;
+}
+
+void ESRenderer::PushRenderFunc(float order, SpriteID sprite, const MTransform &tr, Color tint) {
+    renderer.renderQueues[renderer.currentQueue][order].emplace_back(sprite, tr, tint);
+}
+
+void ESRenderer::Draw() {
+    for(auto const& [order, queue] : renderer.renderQueues[renderer.GetNextQueue()]) {
+        for(auto const& thing : queue) {
+            const Sprite& sprite = SpriteManager::GetSprite(thing.what);
+            const Texture2D& tex = SpriteManager::GetTexture(sprite.texture);
+
+            Vector2 scale(thing.where.scale.x / tex.width, thing.where.scale.y / tex.height);
+            
+            //BeginShaderMode(SpriteManager::GetShader(drawer.shader));
+            DrawTexturePro(tex, sprite.rect, 
+                { thing.where.position.x, thing.where.position.y, thing.where.scale.x * 2.0f, thing.where.scale.y * 2.0f },
+                { thing.where.scale.x, thing.where.scale.y }, thing.where.rotation,  thing.tint);
+            
+            //EndShaderMode();
+        }
+    }
+}
+
+void ESRenderer::Sync() {
+    renderer.currentQueue = renderer.GetNextQueue();
+    renderer.renderQueues[renderer.currentQueue].clear();
+}
